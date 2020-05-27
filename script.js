decaabd082a0d17066ec77f8023bb53ba20c9909
Marci Feiden <mfeiden@gmail.com>
2020-05-27 17:15:06 +0000

    Evaluate user response + update score

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+//global variables
+var answer;
+var score = 0;
+
+//generate questions+answers
 var generateQ = function() {
-  // variables
-  var max = parseInt($('input:radio[name=max]:checked').val(), 10);
+  var max = Number($('input:radio[name=max]:checked').val(), 10);
   var num1 = Math.floor(Math.random() * max) + 1;
   var num2 = Math.floor(Math.random() * max) + 1;
   var op = $('input:radio[name=ops]:checked').val();
@@ -8,8 +12,7 @@ var generateQ = function() {
   var opArrC = ['+', '-', 'x'];
   var operator;
   var question;
-  var answer;
-  // get operator
+  //get operator
   switch (op) {
     case 'b':
       operator = opArrB[Math.floor(Math.random()*opArrB.length)];
@@ -20,10 +23,11 @@ var generateQ = function() {
     default:
       operator = '+';
   }
-  // putting it all together...
+  //putting it all together...
   if (operator === '-' && num1 < num2) {
     question = num2 + ' - ' + num1;
     answer = num2 - num1;
+    //above precludes possibility of negative answer
   } else {
     question = num1 + ' ' + operator + ' ' + num2;
     switch (operator) {
@@ -35,18 +39,39 @@ var generateQ = function() {
         break;
       case 'x':
         answer = num1 * num2;
-    }
+    } 
   };
   $('#Q').text(question);
 };
 
+//compare user input to correct answer
+var checkResponse = function() {
+  if(Number($('#A').val()) === answer) {
+    score += 1;
+    $('#score span').text(score);
+    generateQ();
+    $('#A').val('');
+    
+    
 
+  } 
+};
 
-$(document).ready(function() {
-  $('#start').click(function(){
+//start the game
+var startGame = function() {
     $('#intro').css("display", "none")
     $('#play').removeClass("invisible");
+    $('#A').focus();
     generateQ();
-  });
+};
+
+
+//event listeners+handlers
+$(document).ready(function() {
+  
+  $('#start').click(startGame);
+    
+  
+  $('#A').on('keyup', checkResponse);
 });
   
