9a5dbf2e57bb8de0d56d750148c7b511b5054d4f
Marci Feiden <mfeiden@gmail.com>
2020-05-27 19:42:30 +0000

    Timer + end game functions

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 //global variables
 var answer;
+var timer;
 var score = 0;
+var timeRemaining = 10;
 
 //generate questions+answers
 var generateQ = function() {
+  //variables
   var max = Number($('input:radio[name=max]:checked').val(), 10);
   var num1 = Math.floor(Math.random() * max) + 1;
   var num2 = Math.floor(Math.random() * max) + 1;
@@ -44,25 +47,50 @@ var generateQ = function() {
   $('#Q').text(question);
 };
 
+//end game
+var endGame = function() {
+  $('#end').removeClass("invisible");
+  $('#Q').text('');
+  $('#A').val('');
+  $('#new').click(function() {
+    $('#intro').css("display", "");
+    $('#play').addClass("invisible");
+    $('#end').addClass("invisible");
+    score = 0;
+    timeRemaining = 10;
+  })
+};
+
+//timer
+var updateTime = function (secs) {
+  timeRemaining += secs;
+  $('#timer span').text(timeRemaining);
+  if (timeRemaining === 0) {
+    clearInterval(timer);
+    timer='';
+    endGame();
+  }
+};
+
 //compare user input to correct answer
 var checkResponse = function() {
   if(Number($('#A').val()) === answer) {
     score += 1;
     $('#score span').text(score);
+    updateTime(1);
     generateQ();
     $('#A').val('');
-    
-    
-
-  } 
+  }
 };
 
 //start the game
 var startGame = function() {
-    $('#intro').css("display", "none")
-    $('#play').removeClass("invisible");
-    $('#A').focus();
-    generateQ();
+  $('#score span').text(0);
+  $('#timer span').text(10);
+  $('#intro').css("display", "none")
+  $('#play').removeClass("invisible");
+  $('#A').focus();
+  generateQ();
 };
 
 
@@ -70,8 +98,15 @@ var startGame = function() {
 $(document).ready(function() {
   
   $('#start').click(startGame);
-    
   
-  $('#A').on('keyup', checkResponse);
+  $('#A').on('keyup', function() {
+    if (!timer) {
+      timer = setInterval (function() {
+        updateTime(-1);
+      }, 1000);
+    }
+  checkResponse();
+  });
+  
 });
   
